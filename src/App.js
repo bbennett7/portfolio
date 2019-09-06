fe862f83b290e9bf890c59e76419f62a1aef09d9
bbennett7 <bryn.bennett@gmail.com>
2019-09-06 01:26:45 -0700

    Add menu

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import React from 'react';
 import './App.css';
 
 import Header from './components/Header.js';
+import Menu from './components/Menu.js'
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <Menu />
     </div>
   );
 }
---
+++ src/components/Menu.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+export default class MenuBar extends Component {
+    render() {
+        return(
+            <div className="Menu">
+                <h4>Home</h4>
+                <h4>About</h4>
+                <h4>Erin Boyle Design</h4>
+                <h4>WikiTrash</h4>
+                <h4>CodeCoach</h4>
+                <h4>Bucketlist</h4>
+            </div>
+        )
+    }
+}
