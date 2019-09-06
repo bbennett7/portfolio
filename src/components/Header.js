b7cedb7cd9b2c93d88b50667bbafec7ecc21ae3c
bbennett7 <bryn.bennett@gmail.com>
2019-09-06 00:53:56 -0700

    Layout grid and create Header component

---
+++ src/components/Header.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+export default class Header extends Component {
+    render() {
+        return(
+            <div className="App-header">
+                <h1>Bryn Catherine Bennett</h1>
+                <h3>Full-Stack Software Developer - Problem Solver - Adventurer</h3>
+            </div>
+        )
+    }
+}
