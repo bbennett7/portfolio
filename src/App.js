9dfddab066783aa1473fc4725b68c2ebd1147104
bbennett7 <bryn.bennett@gmail.com>
2019-09-06 02:00:50 -0700

    Start adding social icons

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import './App.css';
-
 import Header from './components/Header.js';
 import Menu from './components/Menu.js'
+
 
 function App() {
   return (
--- src/components/Menu.js
+++ src/components/Menu.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 
+
 export default class MenuBar extends Component {
     render() {
         return(
@@ -10,7 +11,8 @@ export default class MenuBar extends Component {
                 <h4>WikiTrash</h4>
                 <h4>CodeCoach</h4>
                 <h4>Bucketlist</h4>
-            </div>
+                <h4>Contact</h4>
+             </div>
         )
     }
 }
