8af4abd0f8b6957ccc7f86d0151f24df276ad2e3
bbennett7 <bryn.bennett@gmail.com>
2019-09-11 21:13:17 -0700

    Create main content component

--- src/containers/Main.js
+++ src/containers/Main.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 import Menu from '../components/Menu.js'
+import MainContent from '../components/MainContent.js'
 
 export default class Main extends Component {
     render() {
         return(
             <div className="Main">
                 <Menu />
+                <MainContent />
             </div>
         )
     }
