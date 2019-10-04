ac5d195b3c844a644998532c5ca1cc3a08cbb67e
bbennett7 <bryn.bennett@gmail.com>
2019-10-04 13:34:44 -0700

    Add all icons to home

--- src/components/Home.js
+++ src/components/Home.js
@@ -8,6 +8,14 @@ import VHGrey from '../images/grey/vh-grey.jpg';
 import VHColor from '../images/color/vh-color.jpg';
 import MGrey from '../images/grey/m-grey.jpg';
 import MColor from '../images/color/m-color.jpg';
+import TPGrey from '../images/grey/tp-grey.jpg';
+import TPColor from '../images/color/tp-color.jpg';
+import ZGrey from '../images/grey/z-grey.jpg';
+import ZColor from '../images/color/z-color.jpg';
+import RGrey from '../images/grey/r-grey.jpg';
+import RColor from '../images/color/r-color.jpg';
+import HGrey from '../images/grey/h-grey.jpg';
+import HColor from '../images/color/h-color.jpg';
 
 
 export default class Home extends Component {
@@ -18,10 +26,10 @@ export default class Home extends Component {
                 <HomeIcon greyFile={FIGrey} colorFile={FIColor}/>
                 <HomeIcon greyFile={VHGrey} colorFile={VHColor}/>
                 <HomeIcon greyFile={MGrey} colorFile={MColor}/>
-                <HomeIcon which="TreePeople"/>
-                <HomeIcon which="Dog Mom"/>
-                <HomeIcon which="Reader"/>
-                <HomeIcon which="Outdoor Activities"/>
+                <HomeIcon greyFile={TPGrey} colorFile={TPColor}/>
+                <HomeIcon greyFile={ZGrey} colorFile={ZColor}/>
+                <HomeIcon greyFile={RGrey} colorFile={RColor}/>
+                <HomeIcon greyFile={HGrey} colorFile={HColor}/>
             </div>
         )
     }
