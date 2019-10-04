6f5267c4f0783e8a1f479bb9f79e8f180ce5ba29
bbennett7 <bryn.bennett@gmail.com>
2019-10-04 14:38:26 -0700

    Complete home content

--- src/components/Home.js
+++ src/components/Home.js
@@ -22,14 +22,14 @@ export default class Home extends Component {
     render() {
         return(
             <div className="Home">
-                <HomeIcon greyFile={IUGrey} colorFile={IUColor}/>
-                <HomeIcon greyFile={FIGrey} colorFile={FIColor}/>
-                <HomeIcon greyFile={VHGrey} colorFile={VHColor}/>
-                <HomeIcon greyFile={MGrey} colorFile={MColor}/>
-                <HomeIcon greyFile={TPGrey} colorFile={TPColor}/>
-                <HomeIcon greyFile={ZGrey} colorFile={ZColor}/>
-                <HomeIcon greyFile={RGrey} colorFile={RColor}/>
-                <HomeIcon greyFile={HGrey} colorFile={HColor}/>
+                <HomeIcon greyFile={IUGrey} colorFile={IUColor} title="Indiana University" firstLine="The Kelley School of Business" secondLine="The Jacobs School of Music"/>
+                <HomeIcon greyFile={FIGrey} colorFile={FIColor} title="The Flatiron School" firstLine="Full-stack Software Engineering Course" />
+                <HomeIcon greyFile={VHGrey} colorFile={VHColor} title="Velvet Hammer Music and Management" firstLine="Junior Manager for Grammy-award winning and nominated artists"/>
+                <HomeIcon greyFile={MGrey} colorFile={MColor} title="Writer" firstLine="Contributor to The Startup, 500k+ followers" secondLine="Contributor to JavaScript in Plain English, 12.3k+ followers"/>
+                <HomeIcon greyFile={TPGrey} colorFile={TPColor} title="Tree People" firstLine="Volunteer tree planter"/>
+                <HomeIcon greyFile={ZGrey} colorFile={ZColor} title="Animal Lover" firstLine="Dog mom to Zuma, my rescued shepadoodle and baby"/>
+                <HomeIcon greyFile={RGrey} colorFile={RColor} title="Avid Reader" firstLine="Favorite Books:" secondLine="The Dharma Bums by Jack Kerouac" thirdLine="Travels with Charlie by John Steinbeck" fourthLine="Islands in the Stream by Ernest Hemingway"/>
+                <HomeIcon greyFile={HGrey} colorFile={HColor} title="Wellness and Active Lifestyle Enthusiast" firstLine="Yogi" secondLine="Runner" thirdLine="Rock climber" fourthLine="Meitation novice"/>
             </div>
         )
     }
--- src/components/HomeIcon.js
+++ src/components/HomeIcon.js
@@ -6,6 +6,12 @@ export default class HomeIcon extends Component {
             <div className="Home-icon" >
                 <img className="Grey-icon" src={this.props.greyFile} height="150" width="150" />
                 <img className="Color-icon" src={this.props.colorFile} height="150" width="150" />
+
+                <h4 className="Icon-text">{this.props.title}</h4>
+                <h6 className="Icon-text">{this.props.firstLine}</h6>
+                <h6 className="Icon-text">{this.props.secondLine}</h6>
+                <h6 className="Icon-text">{this.props.thirdLine}</h6>
+                <h6 className="Icon-text">{this.props.fourthLine}</h6>
             </div>
         )
     }
