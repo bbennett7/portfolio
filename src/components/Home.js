5bc14d6487da10a3933b80735db47b5a3342568c
bbennett7 <bryn.bennett@gmail.com>
2019-10-04 10:43:27 -0700

    Add fi and vh home icons

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,17 +2,21 @@ import React, { Component } from 'react';
 import HomeIcon from './HomeIcon';
 import IUGrey from '../images/grey/iu-grey.jpg';
 import IUColor from '../images/color/iu-color.jpg';
+import FIGrey from '../images/grey/fi-grey.jpg';
+import FIColor from '../images/color/fi-color.jpg';
+import VHGrey from '../images/grey/vh-grey.jpg';
+import VHColor from '../images/color/vh-color.jpg';
+
 
 export default class Home extends Component {
     render() {
         return(
             <div className="Home">
                 <HomeIcon greyFile={IUGrey} colorFile={IUColor}/>
-                <HomeIcon which="Flatiron"/>
-                <HomeIcon which="VelvetHammer"/>
+                <HomeIcon greyFile={FIGrey} colorFile={FIColor}/>
+                <HomeIcon greyFile={VHGrey} colorFile={VHColor}/>
                 <HomeIcon which="Writer for Medium"/>
                 <HomeIcon which="TreePeople"/>
-                <HomeIcon which="Hack For LA"/>
                 <HomeIcon which="Dog Mom"/>
                 <HomeIcon which="Reader"/>
                 <HomeIcon which="Outdoor Activities"/>
