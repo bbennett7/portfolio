06d1fb87254ceef7fe1ce468e4611a66c39086c2
bbennett7 <bryn.bennett@gmail.com>
2019-10-04 18:07:51 -0700

    Fix bug in hovering over home icons

--- src/components/Home.js
+++ src/components/Home.js
@@ -24,12 +24,12 @@ export default class Home extends Component {
             <div className="Home">
                 <HomeIcon greyFile={IUGrey} colorFile={IUColor} title="Indiana University" firstLine="The Kelley School of Business" secondLine="The Jacobs School of Music"/>
                 <HomeIcon greyFile={FIGrey} colorFile={FIColor} title="The Flatiron School" firstLine="Full-stack Software Engineering Course" />
-                <HomeIcon greyFile={VHGrey} colorFile={VHColor} title="Velvet Hammer Music and Management" firstLine="Junior Manager for Grammy-award winning and nominated artists"/>
+                <HomeIcon greyFile={VHGrey} colorFile={VHColor} title="Velvet Hammer Music and Management" firstLine="Junior manager for Grammy-award winning artists"/>
                 <HomeIcon greyFile={MGrey} colorFile={MColor} title="Writer" firstLine="Contributor to The Startup, 500k+ followers" secondLine="Contributor to JavaScript in Plain English, 12.3k+ followers"/>
                 <HomeIcon greyFile={TPGrey} colorFile={TPColor} title="Tree People" firstLine="Volunteer tree planter"/>
                 <HomeIcon greyFile={ZGrey} colorFile={ZColor} title="Animal Lover" firstLine="Dog mom to Zuma, my rescued shepadoodle and baby"/>
                 <HomeIcon greyFile={RGrey} colorFile={RColor} title="Avid Reader" firstLine="Favorite Books:" secondLine="The Dharma Bums by Jack Kerouac" thirdLine="Travels with Charlie by John Steinbeck" fourthLine="Islands in the Stream by Ernest Hemingway"/>
-                <HomeIcon greyFile={HGrey} colorFile={HColor} title="Wellness and Active Lifestyle Enthusiast" firstLine="Yogi" secondLine="Runner" thirdLine="Rock climber" fourthLine="Meitation novice"/>
+                <HomeIcon greyFile={HGrey} colorFile={HColor} title="Wellness and Active Lifestyle Enthusiast" firstLine="Yogi" secondLine="Runner" thirdLine="Rock climber" fourthLine="Meditation novice"/>
             </div>
         )
     }
