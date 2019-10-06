86bcf80c62b266a1afe5e5d004257dc422767833
bbennett7 <bryn.bennett@gmail.com>
2019-10-05 19:53:07 -0700

    Add contact page content

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,10 +1,14 @@
 import React, { Component } from 'react';
+import Mountain from '../images/Mountain.jpg';
+
 
 export default class Contact extends Component {
     render() {
         return(
             <div className="Contact">
                 <h2 className="Page-header">Contact</h2>
+                <h3 id="Contact-content">If you are interested in hiring a freelance web developer or just want to connect, shoot Bryn a note at <span id="Email">bryn.bennett@gmail.com</span></h3>
+                <img className="Mountain" src={Mountain} />
             </div>
         )
     }
