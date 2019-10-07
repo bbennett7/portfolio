433def045d178a0335e2589d605eb84d7221de9b
bbennett7 <bryn.bennett@gmail.com>
2019-10-07 15:43:06 -0700

    Finish bucketlist and wikitrash content

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -32,7 +32,7 @@ export default class MenuBar extends Component {
             <div className="Menu">
                 <h4 onClick={this.handleOnClick}> <Link to="/home" className={this.getClass("home")}>Home</Link> </h4>
                 <h4 onClick={this.handleOnClick}> <Link to="/about" className={this.getClass("about")}>About</Link></h4>
-                <h4 onClick={this.handleOnClick}> <Link to="/erinboyledesign" className={this.getClass("erinboyledesign")}>Erin Boyle Design</Link> </h4>
+                {/* <h4 onClick={this.handleOnClick}> <Link to="/erinboyledesign" className={this.getClass("erinboyledesign")}>Erin Boyle Design</Link> </h4> */}
                 <h4 onClick={this.handleOnClick}> <Link to="/wikitrash" className={this.getClass("wikitrash")}>WikiTrash</Link> </h4>
                 <h4 onClick={this.handleOnClick}> <Link to="/codecoach" className={this.getClass("codecoach")}>CodeCoach</Link> </h4>
                 <h4 onClick={this.handleOnClick}> <Link to="/bucketlist" className={this.getClass("bucketlist")}>BucketList</Link> </h4>
--- src/components/WikiTrash.js
+++ src/components/WikiTrash.js
@@ -5,10 +5,12 @@ export default class WikiTrash extends Component {
         return(
             <div className="Wiki-trash">
                 <h2 className="Page-header">Wikitrash</h2>
-                <p>Ruby on Rails, React, Redux</p>
                 <h4><a href="https://wikitrash.herokuapp.com" className="App-link" target="blank">Visit WikiTrash</a></h4>
                 <h4><a href="https://github.com/bbennett7/wikitrash" className="App-link" target="blank">GitHub Repository (Front End)</a></h4>
                 <h4><a href="https://github.com/bbennett7/developer-wikitrash" className="App-link" target="blank">GitHub Repository (API)</a></h4>
+                <p>Ruby on Rails, React, Redux</p>
+                <p>WikiTrash is a resource that gathers and provides information on the responsible disposal of different materials. Users can contribute information on these responsible disposal guidelines, vote on the quality and accuracy of information provided by other users, and search the database for information.</p>
+                <p>The front-end is built as a single-page web application using React and Redux, which consumes a separate Ruby on Rails back-end API. The application uses React components and containers, and the Redux store updates in sync with database updates, creating a fluid user experience.</p>
 
                 <iframe width="560" height="315" src="https://www.youtube.com/embed/jskcDg8LbUo" frameborder="0" allow="accelerometer; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
             </div>
