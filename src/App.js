976b739410dae69150b6e89d22c742881ff0d562
bbennett7 <bryn.bennett@gmail.com>
2019-10-09 12:25:50 -0700

    Style mobile and tablet

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import './App.css';
 import './Tablet.css';
+import './Mobile.css';
 import Header from './components/Header.js';
 import Menu from './components/Menu.js'
 
