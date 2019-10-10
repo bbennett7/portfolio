63d9c3069b94ec5e349931048dcdacbeae7f8065
bbennett7 <bryn.bennett@gmail.com>
2019-10-09 22:45:53 -0700

    Style tablet home icons

--- src/components/HomeIcon.js
+++ src/components/HomeIcon.js
@@ -4,8 +4,8 @@ export default class HomeIcon extends Component {
     render() {
         return(
             <div className="Home-icon" >
-                <img className="Grey-icon" src={this.props.greyFile} height="150" width="150" />
-                <img className="Color-icon" src={this.props.colorFile} height="150" width="150" />
+                <div className="Icon"><img className="Grey-icon" src={this.props.greyFile} height="150" width="150" /></div>
+                <div className="Icon"><img className="Color-icon" src={this.props.colorFile} height="150" width="150" /></div>
                 <div id="Icon-text">
                     <h4 className="Icon-text">{this.props.title}</h4>
                     <h6 className="Icon-text">{this.props.firstLine}</h6>
