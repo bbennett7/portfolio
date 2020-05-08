5a4e90eeb30d495cf1d47cb0ba95bbe70bca8baa
bbennett7 <bryn.bennett@gmail.com>
2020-05-08 15:00:38 -0700

    Home icon styling

--- src/components/HomeIcon/HomeIcon.js
+++ src/components/HomeIcon/HomeIcon.js
@@ -5,13 +5,15 @@ export default class HomeIcon extends Component {
   render() {
     return (
       <div className={styles.container}>
-        <div className={styles.icon}>
-          <img className={styles.greyIcon} src={this.props.greyFile} height="150" width="150" />
+        <div className={styles.iconWrapper}>
+          <div className={styles.icon}>
+            <img className={styles.greyIcon} src={this.props.greyFile} height="150" width="150" />
+          </div>
+          <div className={styles.icon}>
+            <img className={styles.colorIcon} src={this.props.colorFile} height="150" width="150" />
+          </div>
         </div>
-        <div className={styles.icon}>
-          <img className={styles.colorIcon} src={this.props.colorFile} height="150" width="150" />
-        </div>
-        <div className={styles.iconText}>
+        <div className={styles.iconTextWrapper}>
           <h4 className={styles.iconText}>{this.props.title}</h4>
           <h6 className={styles.iconText}>{this.props.firstLine}</h6>
           <h6 className={styles.iconText}>{this.props.secondLine}</h6>
