98a6b6b863e70dbd8ff6c7049cd1db9f25a4907b
bbennett7 <bryn.bennett@gmail.com>
2020-05-08 13:05:10 -0700

    Header refactor

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,26 +1,56 @@
-import React, { Component } from 'react';
-import Github from '../../assets/socialicons/github.png';
-import Twitter from '../../assets/socialicons/twitter.png';
-import Linkedin from '../../assets/socialicons/linkedin.png';
-import Medium from '../../assets/socialicons/medium.png';
+import React, { Component } from "react";
+import styles from "./Header.module.scss";
+import Github from "../../assets/socialicons/github.png";
+import Twitter from "../../assets/socialicons/twitter.png";
+import Linkedin from "../../assets/socialicons/linkedin.png";
+import Medium from "../../assets/socialicons/medium.png";
 
 export default class Header extends Component {
-    render() {
-        return(
-            <div className="Header">
-                <div className="Title">
-                    <h1 id="Header">Bryn Catherine Bennett</h1>
-                    <h3 id="Sub-header">Full-Stack Software Developer | Problem Solver | Adventurer</h3>
-                    <h5 id="Tech-list">JavaScript | React | Express | Node | Next | Ruby On Rails | SQL | HTML | CSS | Sass | AWS | Heroku | PostgreSQL | Redis | Microsoft SQL | Firebase | Auth0</h5>
-                </div>
+  render() {
+    return (
+      <div className={styles.container}>
+        <div className={styles.wrapper}>
+          <div className={styles.header}>Bryn Catherine Bennett</div>
+          <div className={styles.subHeader}>
+            Full-Stack Software Developer | Problem Solver | Adventurer
+          </div>
+          <div className={styles.techList}>
+            JavaScript | React | Express | Node | Ruby On Rails | SQL | HTML |
+            CSS | Sass | AWS | Heroku | PostgreSQL | Redis | APIs | Firebase
+          </div>
+        </div>
 
-                <div className="Social-icons">
-                    <a href="https://twitter.com/BrynBennett7" target="_blank"><img src={Twitter} className="social-icon" /></a>
-                    <a href="https://medium.com/@bryn.bennett" target="_blank"><img src={Medium} className="social-icon" /></a>
-                    <a href="https://www.linkedin.com/in/bryncbennett/" target="_blank"><img src={Linkedin} className="social-icon" /></a>
-                    <a href="https://github.com/bbennett7" target="_blank"><img src={Github} className="social-icon" /></a>
-                </div>
-            </div>
-        )
-    }
+        <div className={styles.iconsWrapper}>
+          <a
+            href="https://twitter.com/BrynBennett7"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <img src={Twitter} className={styles.icon} />
+          </a>
+          <a
+            href="https://medium.com/@bryn.bennett"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <img src={Medium} className={styles.icon} />
+          </a>
+          <a
+            href="https://www.linkedin.com/in/bryncbennett/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <img src={Linkedin} className={styles.icon} />
+          </a>
+          <a
+            href="https://github.com/bbennett7"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <img src={Github} className={styles.icon} />
+          </a>
+        </div>
+      </div>
+    );
+  }
 }
