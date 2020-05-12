a0139f177394e2bf161b18b949ef2f1bd90963c0
bbennett7 <bryn.bennett@gmail.com>
2020-05-11 23:18:25 -0700

    Move icon data to imported file

---
+++ src/pages/Home/IconData.js
@@ -0,0 +1,85 @@
+import IUGrey from '../../assets/grey/iu-grey.png';
+import IUColor from '../../assets/color/iu-color.png';
+import VHGrey from '../../assets/grey/vh-grey.png';
+import VHColor from '../../assets/color/vh-color.png';
+import MGrey from '../../assets/grey/m-grey.png';
+import MColor from '../../assets/color/m-color.png';
+import TPGrey from '../../assets/grey/tp-grey.png';
+import TPColor from '../../assets/color/tp-color.png';
+import ZGrey from '../../assets/grey/z-grey.png';
+import ZColor from '../../assets/color/z-color.png';
+import RGrey from '../../assets/grey/r-grey.png';
+import RColor from '../../assets/color/r-color.png';
+import HGrey from '../../assets/grey/h-grey.png';
+import HColor from '../../assets/color/h-color.png';
+import SGrey from '../../assets/grey/s-grey.png';
+import SColor from '../../assets/color/s-color.png';
+import STColor from '../../assets/color/st-color.png';
+import STGrey from '../../assets/grey/st-grey.png';
+
+const data = [
+  {
+    greyFile: IUGrey,
+    colorFile: IUColor,
+    title: 'Indiana University',
+    firstLine: 'The Kelley School of Business',
+    secondLine: 'The Jacobs School of Music'
+  },
+  {
+    greyFile: STGrey,
+    colorFile: STColor,
+    title: 'Stealth Startup',
+    firstLine: 'Full-stack software engineer for a stealth startup in the fintech space.'
+  },
+  {
+    greyFile: SGrey,
+    colorFile: SColor,
+    title: 'Syntx',
+    firstLine: 'Full-stack software engineer building Node.js and React.js applications'
+  },
+  {
+    greyFile: VHGrey,
+    colorFile: VHColor,
+    title: 'Velvet Hammer Music and Management',
+    firstLine: 'Junior manager for Grammy-award winning artists'
+  },
+  {
+    greyFile: MGrey,
+    colorFile: MColor,
+    title: 'Writer',
+    firstLine:
+      'Contributor to Medium publications The Startup (600k+ followers), Better Programming (90k+ followers), and JavaScript in Plain English (12.3k+ followers)'
+  },
+  {
+    greyFile: TPGrey,
+    colorFile: TPColor,
+    title: 'Tree People',
+    firstLine: 'Volunteer tree planter'
+  },
+  {
+    greyFile: ZGrey,
+    colorFile: ZColor,
+    title: 'Animal Lover',
+    firstLine: "Dog mom to Zuma, Bryn's rescued shepadoodle and baby"
+  },
+  {
+    greyFile: RGrey,
+    colorFile: RColor,
+    title: 'Avid Reader',
+    firstLine: 'Favorite Books:',
+    secondLine: 'The Dharma Bums, Jack Kerouac',
+    thirdLine: 'Travels with Charlie, John Steinbeck',
+    fourthLine: 'Islands in the Stream, Ernest Hemingway'
+  },
+  {
+    greyFile: HGrey,
+    colorFile: HColor,
+    title: 'Wellness and Active Lifestyle Enthusiast',
+    firstLine: 'Yogi',
+    secondLine: 'Runner',
+    thirdLine: 'Rock climber',
+    fourthLine: 'Meditation novice'
+  }
+];
+
+export default data;
