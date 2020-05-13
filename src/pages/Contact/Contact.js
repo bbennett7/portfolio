1f94f1309c26f3d6b947ec68497b408801c8d7bf
bbennett7 <bryn.bennett@gmail.com>
2020-05-12 19:53:27 -0700

    Update contact page

--- src/pages/Contact/Contact.js
+++ src/pages/Contact/Contact.js
@@ -1,19 +1,21 @@
 import React, { Component } from 'react';
-import Mountain from '../../assets/Mountain.jpg';
+import styles from './Contact.module.scss';
+import Animation from '../../components/Animation/Animation';
 
 export default class Contact extends Component {
   render() {
     return (
-      <div className="Contact">
-        <h2 className="Page-header">Contact</h2>
-        <h3 id="Contact-content">
-          If you are interested in hiring a freelance web developer or just want to connect, you can
-          send Bryn a note at{' '}
-          <a href="mailto:bryn.bennett@gmail.com" id="Email">
+      <div className={styles.container}>
+        <h2>Contact</h2>
+        <h3>
+          If you would like to connect, send Bryn a note at{' '}
+          <a href="mailto:bryn.bennett@gmail.com" className={styles.email}>
             bryn.bennett@gmail.com
           </a>
+          . She is especially happy to speak with up and coming developers.
         </h3>
-        <img className="Mountain" src={Mountain} />
+        <Animation />
+        {/* <img className="Mountain" src={Mountain} /> */}
       </div>
     );
   }
