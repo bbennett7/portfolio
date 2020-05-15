320bcbc470f6bd8bd6f807ca4b92919e036bfef7
bbennett7 <bryn.bennett@gmail.com>
2020-05-14 22:16:07 -0700

    Update propertii page

--- src/pages/Propertii/Propertii.js
+++ src/pages/Propertii/Propertii.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import styles from './Propertii.module.scss';
 import { ReactComponent as Github } from '../../assets/icons8-github.svg';
 import { ReactComponent as Web } from '../../assets/icons8-website.svg';
 import PropertiiHome from '../../assets/PropertiiHome.png';
@@ -8,34 +9,43 @@ import PropertiiMgmtHome from '../../assets/PropertiiBuildingMgmtHome.png';
 export default class Propertii extends Component {
   render() {
     return (
-      <div className="Propertii">
-        <div className="componentHeaderWrapper">
-          <h2 className="Page-header">Propertii</h2>
-          <a
-            href="https://propertii.us/"
-            className="App-link"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Web className="linkIcon" />
-          </a>
-
-          <a
-            href="https://github.com/bbennett7/properti-web-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Github className="linkIcon" /> (Frontend)
-          </a>
-          <a
-            href="https://github.com/bbennett7/properti-server"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Github className="linkIcon" /> (Backend)
-          </a>
+      <div className={styles.container}>
+        <div className={styles.componentHeaderWrapper}>
+          <h2 className={styles.header}>Propertii</h2>
+          <div className={styles.iconsWrapper}>
+            <div className={styles.iconWrapper}>
+              <a
+                href="https://propertii.us/"
+                className="App-link"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <Web className={styles.icon} />
+              </a>
+            </div>
+            <div className={styles.iconWrapper}>
+              <a
+                href="https://github.com/bbennett7/properti-web-app"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <Github className={styles.icon} />
+              </a>
+              FE
+            </div>
+            <div className={styles.iconWrapper}>
+              <a
+                href="https://github.com/bbennett7/properti-server"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <Github className={styles.icon} />
+              </a>
+              BE
+            </div>
+          </div>
         </div>
-        <p>React, Express, PostgreSQL, Yelp API, Firebase</p>
+        <p className={styles.tech}>React, Express, PostgreSQL, Yelp API, Firebase</p>
         <p>
           Propertii is a web application that allows building managers and residents to connect on
           and track building repairs. Built using React, Express, and PostgreSQL, residents can open
@@ -47,9 +57,15 @@ export default class Propertii extends Component {
           Yelp API), change the status of the repair, and contact the resident.
         </p>
 
-        <img src={PropertiiHome} className="projectImg" />
-        <img src={PropertiiAccount} className="projectImg" />
-        <img src={PropertiiMgmtHome} className="projectImg" />
+        <div className={styles.imageWrapper}>
+          <img src={PropertiiHome} className={styles.image} />
+        </div>
+        <div className={styles.imageWrapper}>
+          <img src={PropertiiAccount} className={styles.image} />
+        </div>
+        <div className={styles.imageWrapper}>
+          <img src={PropertiiMgmtHome} className={styles.image} />
+        </div>
       </div>
     );
   }
