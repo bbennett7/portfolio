9b95d8880c0b2913996ec6b7f21fac6373c87226
bbennett7 <bryn.bennett@gmail.com>
2020-05-20 19:54:22 -0700

    Update eb design page

--- src/pages/EBDesign/EBDesign.js
+++ src/pages/EBDesign/EBDesign.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import styles from './EBDesign.module.scss';
 import EBDesignImg from '../../assets/EB-Design.png';
 import { ReactComponent as Github } from '../../assets/icons8-github.svg';
 import { ReactComponent as Web } from '../../assets/icons8-website.svg';
@@ -6,17 +7,27 @@ import { ReactComponent as Web } from '../../assets/icons8-website.svg';
 export default class EBDesign extends Component {
   render() {
     return (
-      <div className="EB-Design">
-        <div className="componentHeaderWrapper">
+      <div className={styles.container}>
+        <div className={styles.headerWrapper}>
           <h2 className="Page-header">Erin Boyle Design</h2>
-          <a href="https://www.erinboyledesign.com/" target="_blank" rel="noopener noreferrer">
-            <Web className="linkIcon" />
-          </a>
-          <a href="https://github.com/bbennett7/ebdesign" target="_blank" rel="noopener noreferrer">
-            <Github className="linkIcon" />
-          </a>
+          <div>
+            <div className={styles.iconWrapper}>
+              <a href="https://www.erinboyledesign.com/" target="_blank" rel="noopener noreferrer">
+                <Web className={styles.icon} />
+              </a>
+            </div>
+            <div className={styles.iconWrapper}>
+              <a
+                href="https://github.com/bbennett7/ebdesign"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <Github className={styles.icon} />
+              </a>
+            </div>
+          </div>
         </div>
-        <p>React</p>
+        <p className={styles.tech}>React</p>
         <p>
           Built using the JavaScript React library and Bootstrap, this web application showcases the
           work of renowned Los Angeles interior design firm, Erin Boyle Design. From multi-family
@@ -24,7 +35,9 @@ export default class EBDesign extends Component {
           through React components, vanilla CSS styling and Bootstrap for elements such as the
           navigation.
         </p>
-        <img src={EBDesignImg} className="projectImg" />
+        <div>
+          <img src={EBDesignImg} className={styles.image} />
+        </div>
       </div>
     );
   }
