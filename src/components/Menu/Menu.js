6464c102e8c4b556b3e979b8d2a31760133581f4
bbennett7 <bryn.bennett@gmail.com>
2020-05-20 19:59:09 -0700

    Update menu styling

--- src/components/Menu/Menu.js
+++ src/components/Menu/Menu.js
@@ -1,101 +1,46 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import styles from './Menu.module.scss';
 import { ReactComponent as Menu } from '../../assets/icons8-menu.svg';
 
-export default class MenuBar extends Component {
-  state = {
-    path: '',
-    mobileDisplay: 'Display-false'
-  };
+const MenuBar = () => {
+  const [mobileDisplay, setMobileDisplay] = useState(false);
 
-  handleOnClick = event => {
-    const path = event.target.innerHTML
-      .toLowerCase()
-      .replace(' ', '')
-      .replace(' ', '');
-
-    this.setState({
-      path,
-      mobileDisplay: 'Display-false'
-    });
-  };
-
-  getClass = component => {
-    if (this.state.path === component) {
-      return styles.active;
+  const menuDisplay = () => {
+    if (!mobileDisplay) {
+      return setMobileDisplay(true);
     }
-    return styles.inactive;
+    return setMobileDisplay(false);
   };
 
-  menuDisplay = () => {
-    console.log(this.state);
-    let status = '';
-
-    if (this.state.mobileDisplay === 'Display-false') {
-      status = 'Display-true';
-    } else if (this.state.mobileDisplay === 'Display-true') {
-      status = 'Display-false';
-    }
-
-    this.setState({
-      ...this.state,
-      mobileDisplay: status
-    });
-  };
-
-  render() {
-    return (
-      <div className={styles.container}>
-        <Menu className={styles.mobileMenu} onClick={this.menuDisplay} />
-        <div className={styles.wrapper} id={this.state.mobileDisplay}>
-          <div onClick={this.handleOnClick}>
-            {' '}
-            <Link to="/home" className={`${this.getClass('home')} ${styles.menuLink}`}>
-              Home
-            </Link>{' '}
-          </div>
-          <div onClick={this.handleOnClick}>
-            {' '}
-            <Link to="/about" className={`${this.getClass('about')} ${styles.menuLink}`}>
-              About
-            </Link>
-          </div>
-          <div onClick={this.handleOnClick}>
-            {' '}
-            <Link to="/propertii" className={`${this.getClass('propertii')} ${styles.menuLink}`}>
-              Propertii
-            </Link>{' '}
-          </div>
-          <div onClick={this.handleOnClick}>
-            {' '}
-            <Link
-              to="/erinboyledesign"
-              className={`${this.getClass('erinboyledesign')} ${styles.menuLink}`}
-            >
-              Erin Boyle Design
-            </Link>{' '}
-          </div>
-          <div onClick={this.handleOnClick}>
-            {' '}
-            <Link to="/wikitrash" className={`${this.getClass('wikitrash')} ${styles.menuLink}`}>
-              WikiTrash
-            </Link>{' '}
-          </div>
-          <div onClick={this.handleOnClick}>
-            {' '}
-            <Link to="/codecoach" className={`${this.getClass('codecoach')} ${styles.menuLink}`}>
-              CodeCoach
-            </Link>{' '}
-          </div>
-          <div onClick={this.handleOnClick}>
-            {' '}
-            <Link to="/contact" className={`${this.getClass('contact')} ${styles.menuLink}`}>
-              Contact
-            </Link>{' '}
-          </div>
-        </div>
+  return (
+    <div className={styles.container}>
+      <Menu className={styles.mobileMenu} onClick={menuDisplay} />
+      <div className={styles.wrapper} id={mobileDisplay}>
+        <Link to="/" className={styles.menuLink}>
+          Home
+        </Link>
+        <Link to="/about" className={styles.menuLink}>
+          About
+        </Link>
+        <Link to="/propertii" className={styles.menuLink}>
+          Propertii
+        </Link>
+        <Link to="/erinboyledesign" className={styles.menuLink}>
+          Erin Boyle Design
+        </Link>
+        <Link to="/wikitrash" className={styles.menuLink}>
+          WikiTrash
+        </Link>
+        <Link to="/codecoach" className={styles.menuLink}>
+          CodeCoach
+        </Link>
+        <Link to="/contact" className={styles.menuLink}>
+          Contact
+        </Link>
       </div>
-    );
-  }
-}
+    </div>
+  );
+};
+
+export default MenuBar;
